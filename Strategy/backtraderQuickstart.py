53a0018f5fa92ca7aa53043e5e363045be464133
Elliot Warner <warner.e@windowslive.com>
2021-02-01 16:33:03 +1300

    started a proper file structure

--- Strategy/backtraderQuickstart.py
+++ Strategy/backtraderQuickstart.py
@@ -18,7 +18,7 @@ class TestStrategy(bt.Strategy):
     )
 
     def log(self, txt, dt=None, doprint=False):
-        ''' Logging function fot this strategy'''
+        """ Logging function fot this strategy """
         if self.params.printlog or doprint:
             dt = dt or self.datas[0].datetime.date(0)
             print('%s, %s' % (dt.isoformat(), txt))
